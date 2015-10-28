0781242f4b1ea7ce97f3e48adb2b282359bab7f8
Alessio Treglia <alessio@debian.org>
2015-10-28 22:08:19 +0000

    Import Debian changes 1.1.10-2
    libmtp (1.1.10-2) unstable; urgency=medium
    
      * debian/apport/source_libmtp.py:
        - updated apport hook to work, import what we need, mention the
          current soname version and don't include hal info. Thanks to
          Sebastien Bacher for the patch. (Closes: #802601) (LP: #1172123)
      * Add Noka C5-00's alternative ID (Closes: #728088)

--- debian/apport/source_libmtp.py
+++ debian/apport/source_libmtp.py
@@ -9,30 +9,31 @@ the full text of the license.
 (c) 2009 Sense Hofstede <sense@qense.nl>
 '''
 
+import apport.hookutils
+
 def add_info(report, ui):
     
     ui.information('Please make sure the affected device is connected and on before continuing.')
     
-    attach_related_packages(report, [
-        "hal",
+    apport.hookutils.attach_related_packages(report, [
         "udev",
         ])
         
     # Try using the mtp-detect command to obtain more information
-    if command_available("mtp-detect"):
-        report['MTPDetect'] = command_output("mtp-detect")
+    if apport.hookutils.command_available("mtp-detect"):
+        report['MTPDetect'] = apport.hookutils.command_output("mtp-detect")
     else:
         ui.information("Please install the package 'mtp-tools' so we can gather "\
                         "more detailed debugging information. Afterwards, rerun " \
                         "the command 'ubuntu-bug libmtp8' or add more information "\
                         "to an existing bug report by running the command "\
-                        "'apport-collect -p libmtp8 '<bugnumber>', replacing "\
+                        "'apport-collect -p libmtp9 '<bugnumber>', replacing "\
                         "<bugnumber> with the number of your bug report.")
     
     # Obtain information about changes to udev configuration files
-    attach_conffiles(report, "udev")
+    apport.hookutils.attach_conffiles(report, "udev")
     # Attach the udev log file
-    attach_file_if_exists(report, '/var/log/udev', 'UdevLog')
+    apport.hookutils.attach_file_if_exists(report, '/var/log/udev', 'UdevLog')
     
     # Get all connected USB devices
-    report['USBDevices'] = usb_devices()
+    report['USBDevices'] = apport.hookutils.usb_devices()
